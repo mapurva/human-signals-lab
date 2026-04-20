abfc710eea98b7e67f0cd362202cf50f730c135c
mapurva <apurvamehta2015@gmail.com>
2026-04-20 10:21:46 +0530

    feat: initial project setup with intimacy analyzer v1

---
+++ python/intimacy_v1.py
@@ -0,0 +1,16 @@
+def closeness_score(message):
+    words = message.lower().split()
+    score = 0
+    
+    emotional_words = ["miss", "love", "feel", "want"]
+    
+    for w in words:
+        if w in emotional_words:
+            score += 2
+    
+    score += len(words) * 0.5
+    
+    return score
+
+msg = input("Enter message: ")
+print("Closeness Score:", closeness_score(msg))
