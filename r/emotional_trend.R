29961a573aeaf3e6b1c1ced0a369adda98b940aa
mapurva <apurvamehta2015@gmail.com>
2026-04-22 09:18:25 +0530

    feat: add emotional trend analyzer in R

---
+++ r/emotional_trend.R
@@ -0,0 +1,24 @@
+messages <- readLines("data/messages.txt")
+
+emotional_words <- c("miss", "love", "feel", "want", "need", "thinking")
+
+closeness_score <- function(msg) {
+  words <- strsplit(tolower(msg), " ")[[1]]
+  score <- 0
+  
+  for (w in words) {
+    if (w %in% emotional_words) {
+      score <- score + 2
+    }
+  }
+  
+  score <- score + length(words) * 0.5
+  return(score)
+}
+
+scores <- sapply(messages, closeness_score)
+
+print(data.frame(messages, scores))
+
+plot(scores, type="o", main="Emotional Trend",
+     xlab="Message Index", ylab="Score")
